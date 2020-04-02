3c59654dd376ae5e169b11de1399eab95829a9d0
Hannah Werman <hanna.w28@gmail.com>
2020-04-02 11:19:28 -0400

    8) core concept - middleware and error handling

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,11 @@
+exports.myMiddleware = (req, res, next) => {
+    console.log('my middleware')
+    req.name = 'Hannah';
+    res.cookie('name', 'Hannah is cool', { maxAge: 9000000 })
+    next();
+}
+
 exports.homePage = (req, res) => {
+    console.log(req.name);
     res.render('index');
 }
