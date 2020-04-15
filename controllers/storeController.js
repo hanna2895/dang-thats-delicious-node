992f457fc0cecf121d6074e4c2eed0db92f76af9
Hannah Werman <hanna.w28@gmail.com>
2020-04-15 12:53:09 -0400

    19) routing and templating single stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -78,3 +78,9 @@ exports.updateStore = async (req, res) => {
     // 2. redirect them to the store and tell them it worked
     
 }
+
+exports.getStoreBySlug = async (req, res, next) => {
+    const store = await Store.findOne({ slug: req.params.slug });
+    if(!store) return next();
+    res.render('store', { store, title: store.name })
+}
